0d848b897500ffa4cf5b122b48be402fd008e918
mmz <zoumeng1994@163.com>
2016-08-30 22:20:04 +0800

    add function BuildTree

--- node_list/model.c
+++ node_list/model.c
@@ -1,11 +1,220 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct set
+{
+	int id;
+	int eleNum;
+	int freSep;
+	int element[150];
+	struct set *next;
+}set;
+set *setHead = NULL;
+int ReadData(char *path)
+{
+	FILE *fr;
+	char buf[300];
+	int id = 0;
+
+	set *pre = NULL;
+	set *cur = NULL;
+	int totalNum = 0;
+
+	fr = fopen(path, "r");
+	int time = 0;
+	while (fgets(buf, sizeof(buf), fr) && time < 5){
+		time ++;
+		//printf("%s", buf);
+		cur = (set *)malloc(sizeof(set));
+		cur->next = NULL;
+		cur->id = id;
+		cur->eleNum = 0;
+		if (setHead == NULL){
+			setHead = cur;
+			pre = cur;
+		}
+		else{
+			pre->next = cur;
+			pre = cur;
+		}
+		for (int i = 0; strchr(buf, ' '); i++){
+			cur->eleNum = i + 1;
+			sscanf(buf, "%d", &cur->element[i]);
+			//printf("cur->element[%d]= %d\n", i, cur->element[i]);
+			//printf("buf:%s\n", buf);
+			char *firstBlank = strchr(buf, ' ');
+			int blankPos = firstBlank - buf;
+			int strLen = strlen(buf);
+			char tempBuf[300];
+			memset(tempBuf, '\0', 300);
+			//printf("blankPos:%d\n", blankPos);
+			//printf("strLen:%d\n", strLen);
+			//printf("cpyStart:%d\n", buf + blankPos + 1);
+			//printf("cpyLen:%d\n", strLen - blankPos);
+			strncpy(tempBuf, buf + blankPos + 1, strLen - blankPos);
+			tempBuf[strLen - blankPos] = '\0';
+			memset(buf, '\0', strLen);
+			strcpy(buf, tempBuf);
+			//printf("buf:%s\n",buf);	
+		}
+		id++;
+		totalNum += cur->eleNum;
+		/*
+		printf("id: %d: ",id-1);
+		for (int i = 0; i < cur->eleNum; i++)
+		printf("%d,", cur->element[i]);
+		printf("\n");
+		*/
+	}
+	fclose(fr);
+
+	return totalNum;
+}
+
+int itemCount[3000] = { 0 };
+void CountData(){
+	for (set *setCur = setHead; setCur != NULL; setCur = setCur->next){
+		for (int i = 0; i < setCur->eleNum; i++){
+			itemCount[setCur->element[i]] += 1;
+		}
+	}
+	/*
+	for (int i = 0; i < 3000; i++)
+		printf("%d: %d times\n", i, itemCount[i]);
+	*/
+}
+
+void RearrangeData(int totalNum, float threshold){
+	for (set *setCur = setHead; setCur != NULL; setCur = setCur->next){
+		for (int i = 0; i < setCur->eleNum; i++){
+			for (int j = i + 1; j < setCur->eleNum; j++){
+				if (itemCount[setCur->element[i]] < itemCount[setCur->element[j]]){
+					int temp = setCur->element[i];
+					setCur->element[i] = setCur->element[j];
+					setCur->element[j] = temp;
+				}
+			}
+		}
+	}
+}
+
+void CheckSet()
+{
+	for (set *setCur = setHead; setCur != NULL; setCur = setCur->next){
+		printf("id:%d\n",setCur->id);
+		printf("num:%d\n",setCur->eleNum);
+		for (int i = 0; i < setCur->eleNum; i++){
+			printf("%d(%d) ", setCur->element[i], itemCount[setCur->element[i]]);
+		}
+		printf("\n\n");
+	}
+}
+
+typedef struct nodeRec
+{
+	int setId;
+	struct nodeRec *next;
+}nodeRec;
+typedef struct node
+{
+	int nodeId;
+	int DLR;
+	int LRD;
+	struct nodeRec *firstRec;
+	struct nodeRec *lastRec;
+	struct node *son;
+	struct node *bro;
+	leaf *sonleaf;
+}node;
+node *root = NULL;
+typedef struct leafRec
+{
+	int leafId;
+	nodeRec *firstRec;
+	nodeRec *lastRec;
+	struct leafRec *next;
+}leafRec;
+typedef struct leaf
+{
+	int DLR;
+	int LRD;
+	int leafRecNum;
+	leafRec *firstNotFre;
+}leaf;
+void BuildTree()
+{
+	node *nodePre = NULL;
+	node *nodeLeftBro = NULL;
+	nodeRec *newRec = NULL;
+	for (set *setCur = setHead; setCur != NULL; setCur = setCur->next){
+		node *nodeCur = root;
+		for (int i = 0; i < setCur->freSep; i++){
+			if (nodeCur == NULL){
+				nodeCur = (node *)malloc(sizeof(node));
+				nodeCur->bro = NULL;
+				nodeCur->firstRec = (nodeRec *)malloc(sizeof(nodeRec));
+				nodeCur->firstRec->setId = setCur->id;
+				nodeCur->firstRec->next = NULL;
+				nodeCur->lastRec = nodeCur->firstRec;
+				nodeCur->nodeId = setCur->element[i];
+				nodeCur->son = NULL;
+				nodeCur->sonleaf = NULL;
+				if (root == NULL){
+					root = nodeCur;
+					nodePre = nodeCur;
+				}
+				else{
+					nodePre->son = nodeCur;
+					nodePre = nodeCur;
+				}
+			}
+			else{
+				for (; nodeCur != NULL && nodeCur->nodeId != setCur->element[i]; nodeLeftBro = nodeCur, nodeCur = nodeCur->bro);
+				if (nodeCur == NULL){
+					nodeCur = (node *)malloc(sizeof(node));
+					nodeCur->bro = NULL;
+					nodeCur->firstRec = (nodeRec *)malloc(sizeof(nodeRec));
+					nodeCur->firstRec->setId = setCur->id;
+					nodeCur->firstRec->next = NULL;
+					nodeCur->lastRec = nodeCur->firstRec;
+					nodeCur->nodeId = setCur->element[i];
+					nodeCur->son = NULL;
+					nodeCur->sonleaf = NULL;
+					nodeLeftBro->bro = nodeCur;
+				}
+				else{
+					newRec = (nodeRec *)malloc(sizeof(nodeRec));
+					newRec->setId = setCur->id;
+					newRec->next = NULL;
+					nodeCur->lastRec->next = newRec;
+					nodeCur->lastRec = newRec;
+				}
+				nodePre = nodeCur;
+			}
+			if (i < setCur->freSep - 1)
+				nodeCur = nodePre->son;
+			else
+				nodeCur = nodePre->sonleaf;
+		}
+		for (int i = setCur->freSep; i < setCur->eleNum; i++){
+			//TODO
+		}
+	}
+}
 int main()
 {
-	ReadData();
+	char *filePath = ".//data//mushroom.dat";
+	int totalNum = 0;
+	float threshold = 0.25;
+
+	totalNum = ReadData(filePath);
 	CountData();
-	RearrangeData();
+	CheckSet();
+	RearrangeData(totalNum,threshold);
+	CheckSet();
 	BuildTree();
+	//DLR();
+	//LRD();
+	system("pause");
 	return 0;
 }
