846dce386c07581611631bfa8076a1b1946ee234
mmz <zoumeng1994@163.com>
2016-09-10 13:01:27 +0800

    finish BuildTree & DLR & LRD

--- node_list/model3.cpp
+++ node_list/model3.cpp
@@ -10,13 +10,13 @@ using namespace std;
 
 map<int, vector<int>> setData;
 
-void ReadFile(string path)
+int ReadFile(string path)
 {
 	ifstream in(path);
 	string line;
 	int setId;
 	int maxnum = 0;
-	for (setId = 0; getline(in, line); setId++){
+	for (setId = 0; getline(in, line) && setId<5; setId++){
 		istringstream li(line);
 		vector<int> temp;
 		int data;
@@ -25,10 +25,11 @@ void ReadFile(string path)
 		}
 
 	}
+	return setId;
 }
 
 int itemCount[1000] = { 0 };
-int CountData()
+void CountData()
 {
 	int total = 0;
 	memset(itemCount, 0, 1000 * sizeof(int));
@@ -36,11 +37,11 @@ int CountData()
 		for (int j = 0; j < setData[i].size(); j++)
 			itemCount[setData[i][j]] += 1;
 	//cout << "CountData Done--------" << endl;
-	return total;
+	return;
 }
 
 bool compare(int a, int b){
-	return ((itemCount[a] > itemCount[b]) && (a < b));
+	return ((itemCount[a] > itemCount[b]));
 }
 
 void RearrangeData()
@@ -48,15 +49,16 @@ void RearrangeData()
 	for (int i = 0; i < setData.size(); i++)
 		sort(setData[i].begin(), setData[i].end(), compare);
 	cout << "RearrangeData Done--------" << endl;
-
+	
 	for (int i = 0; i < setData.size(); i++){
 		cout << "after:";
 		for (int j = 0; j < setData[i].size(); j++)
 			cout << setData[i][j] << "(" << itemCount[setData[i][j]] << ") ";
 		cout << endl;
 	}
-
+	
 }
+
 class node
 {
 public:
@@ -70,7 +72,7 @@ public:
 	}
 };
 
-node* BuildTree()
+node* BuildTree(float fre)
 {
 	node *root = new node;
 	node *nodeCur = NULL;
@@ -82,6 +84,12 @@ node* BuildTree()
 				nodeCur = new node;
 				nodeCur->nodeRec[aset.second[i]].push_back(aset.first);
 				nodePre->son.push_back(nodeCur);
+				if (itemCount[aset.second[i]] < fre){
+					nodeCur->leaf = 1;
+				}
+				else{
+					nodePre = nodeCur;
+				}
 			}
 			else{//当前路径往下还有节点
 				int find = 0;
@@ -90,14 +98,40 @@ node* BuildTree()
 					if ((**vi).nodeRec.find(aset.second[i])!=(**vi).nodeRec.end()){//id相同的节点已经插入树中，不需新建节点，只需在原有节点上添上set id
 						(**vi).nodeRec[aset.second[i]].push_back(aset.first);
 						find = 1;
+						if (itemCount[aset.second[i]] >= fre){
+							nodePre = (*vi);
+						}
 						break;
 					}
 				}
-				if (find == 0){//此id节点还没有插入到树中，新建节点并插入
-					//如果当前id是非频繁项，要找当前的son中有没有叶子节点，没有就新建，有就直接添加id
-					nodeCur = new node;
-					nodeCur->nodeRec[aset.second[i]].push_back(aset.first);
-					nodePre->son.push_back(nodeCur);
+				if (find == 0){//此id节点还没有插入到树中,需要判断此id是不是非频繁的。
+					int findLeaf = 0;
+
+					if (itemCount[aset.second[i]] < fre){//如果当前id是非频繁项，要找当前的son中有没有叶子节点，没有就新建，有就直接添加id
+						vector<node *>::iterator vii;
+						for (vii = nodePre->son.begin(); vii != nodePre->son.end(); ++vii){
+							if ((**vii).leaf == 1){
+								findLeaf = 1;
+								break;
+							}
+						}
+						if (findLeaf == 1){//已经建好叶子节点，直接在叶子节点里添加新的纪录
+							(**vii).nodeRec[aset.second[i]].push_back(aset.first);
+						}
+						else{//没有叶子节点，需要新建叶子节点
+							nodeCur = new node;
+							nodeCur->leaf = 1;
+							nodeCur->nodeRec[aset.second[i]].push_back(aset.first);
+							nodePre->son.push_back(nodeCur);
+						}
+
+					}
+					else{//当前id是频繁项，新建节点插入树中
+						nodeCur = new node;
+						nodeCur->nodeRec[aset.second[i]].push_back(aset.first);
+						nodePre->son.push_back(nodeCur);
+						nodePre = nodeCur;
+					}
 				}
 			}
 		}
@@ -105,11 +139,85 @@ node* BuildTree()
 	return root;
 }
 
+int index = 0;
+void ResetIndex(){
+	index = 0;
+}
+void DLR(node *cur)
+{
+	if (cur == NULL){
+		return;
+	}
+	cur->DLR = index;
+	index = index + 1;
+	vector<node *>::iterator vi;
+	for (vi = cur->son.begin(); vi != cur->son.end(); ++vi){
+		DLR(*vi);
+	}
+	return;
+}
+
+void LRD(node *cur)
+{
+	if (cur->son.empty() == true){
+		cur->LRD = index;
+		index = index + 1;
+		return;
+	}
+	vector<node *>::iterator vi;
+	for (vi = cur->son.begin(); vi != cur->son.end(); ++vi){
+		LRD(*vi);
+	}
+	cur->LRD = index;
+	index = index + 1;
+	return;
+}
+
+void CheckTree(node * cur){
+	if (cur == NULL){
+		return;
+	}
+	cout << "(" << cur->DLR <<","<< cur->LRD << ")";
+	if (cur->leaf == 0){
+		cout << "node";
+	}
+	else{
+		cout << "leaf";
+	}
+	for (auto elem : cur->nodeRec){
+		cout << elem.first << ":";
+		for (int i = 0; i < elem.second.size(); i++){
+			cout << elem.second[i] << " ";
+		}
+		cout << endl;
+	}
+	vector<node *>::iterator vi;
+	for (vi = cur->son.begin(); vi != cur->son.end(); ++vi){
+		CheckTree(*vi);
+	}
+	return;
+	return;
+}
+
 int main(){
 	
+	map<string, float> data_fre = { { ".//data//mushroom.dat", 0.25 }, { ".//data//accidents.dat", 0.5 }, { ".//data//T10I4D100K.dat", 0.005 } };
 	string path = ".//data//mushroom.dat";
-	ReadFile(path);
-	node * root = BuildTree();
+	float fre = 0.25;
+	float baseFre;
+	int setNum;
+	
+	setNum = ReadFile(path);
+	CountData();
+	RearrangeData();
+	baseFre = setNum * fre;
+	cout << "频繁项最低支持度：" <<baseFre<< endl;
+	node * root = BuildTree(baseFre);
+	ResetIndex();
+	DLR(root);
+	ResetIndex(); 
+	LRD(root);
+	CheckTree(root);
 	system("pause");
 	return 0;
 }
